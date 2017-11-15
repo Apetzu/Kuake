198b8a2d51d29a24aaec49a1d41e7bedf12bb3ac
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2017-11-16 01:02:04 +0200

    More player movement

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -4,58 +4,43 @@ using System.Collections.Generic;
 
 public class playerController : MonoBehaviour {
 
-    Camera mainCamera;
-    public float mouseSensitivity = 1f;
+    Camera camera;
+    public float mouseSensitivity = 5;
     public CursorLockMode cursorState = CursorLockMode.None;
-    public float movAccel = 5f;
-    public float jumpAccel = 50f;
+    public float movAccel = 10;
+    public float jumpAccel = 300;
+    BoxCollider bc;
     Rigidbody rb;
+    int jump = 0;
 
     void Awake()
     {
-        mainCamera = transform.Find("playerCamera").GetComponent<Camera>();
+        camera = transform.Find("playerCamera").GetComponent<Camera>();
         Cursor.lockState = cursorState;
         rb = GetComponent<Rigidbody>();
-    }
-
-    void Update()
-    {
-        /*float cameraXRot = mainCamera.transform.localRotation.eulerAngles.x;
-        float mainCameraEulerFix;
-
-        if (cameraXRot >= 270)
-            mainCameraEulerFix = cameraXRot - 360;
-        else
-            mainCameraEulerFix = cameraXRot;
-
-        transform.rotation = Quaternion.Euler (0, transform.rotation.eulerAngles.y + mouseDelta.x, 0);
-        mainCamera.transform.localRotation = Quaternion.Euler(Mathf.Clamp((-mainCameraEulerFix + mouseDelta.y) * -1, -90, 90), 0, 0);*/
+        bc = GetComponent<BoxCollider>();
     }
 
     void FixedUpdate()
     {
-       /* int jump = 0;
-
-        if (axis2Bool("Jump")) 
-        {
-            if (Physics.Raycast (new Vector3 (transform.position.x, Mathf.Round(transform.position.y * 100) / 100 - GetComponent<CapsuleCollider> ().height / 2, transform.position.z), Vector3.down, 0.01f, ~(1 << LayerMask.NameToLayer("Player"))))
-                jump = 1;
-        }
-
-        rb.AddRelativeForce(Vector3.Scale(new Vector3 (Input.GetAxisRaw ("Horizontal"), jump, Input.GetAxisRaw ("Vertical")).normalized, new Vector3(movAccel * rb.mass, jumpAccel, movAccel * rb.mass)));*/
-
-        float cameraXRot = mainCamera.transform.localRotation.eulerAngles.x;
-        float mainCameraEulerFix;
-
-        if (cameraXRot >= 270)
-            mainCameraEulerFix = cameraXRot - 360;
-        else
-            mainCameraEulerFix = cameraXRot;
+        rb.AddRelativeForce(Vector3.Scale(new Vector3 (Input.GetAxisRaw ("Horizontal"), jump, Input.GetAxisRaw ("Vertical")).normalized, new Vector3(movAccel * rb.mass, jumpAccel, movAccel * rb.mass)));
 
         Vector2 mouseDelta = mouseMovement() * mouseSensitivity;
 
         rb.MoveRotation(Quaternion.Euler(0, rb.rotation.eulerAngles.y + mouseDelta.x, 0));
-        mainCamera.transform.localRotation = Quaternion.Euler(Mathf.Clamp((-mainCameraEulerFix + mouseDelta.y) * -1, -90, 90), 0, 0);
+        //mainCamera.transform.Rotate(new Vector3(-mouseDelta.y, 0, 0)); This doesn't have clamping capability
+        camera.transform.localRotation = Quaternion.Euler(strangeAxisClamp(-mouseDelta.y + camera.transform.localRotation.eulerAngles.x, 90, 270), 0, 0);
+
+        jump = 0;
+    }
+
+    public float strangeAxisClamp(float value, float limit1, float limit2)
+    {
+        if (value > limit1 && value < 180)
+            value = limit1;
+        else if (value > 180 && value < limit2)
+            value = limit2;
+        return value;
     }
 
     public Vector2 mouseMovement()
